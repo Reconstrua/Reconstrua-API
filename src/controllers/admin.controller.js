2cf5adadb72abe1d3dadc7b0ca2f900eb2fd38cb
Paulo Ricardo <pr741234@gmail.com>
2024-05-06 09:27:12 -0300

    feat: Create admin CRUD controllers
    Creates controllers for the register, get, update and delete admin methods

---
+++ src/controllers/admin.controller.js
@@ -0,0 +1,39 @@
+import { AdminService } from "../services/admin.service.js";
+
+const instanceAdminService = new AdminService();
+
+export const registerAdmin = async (req, res) => {
+  const { username, password, token: providedToken } = req.body;
+  await instanceAdminService.registerAdmin(username, password, providedToken);
+
+  return res.status(201).json({
+    message: "Admin criado com sucesso.",
+  });
+};
+
+export const getAllAdmins = async (req, res) => {
+  const adminsList = await instanceAdminService.getAllAdmins();
+
+  return res.status(200).json({
+    admins: adminsList,
+  });
+};
+
+export const updateAdmin = async (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  await instanceAdminService.updateAdmin(id, body);
+
+  return res.status(200).json({
+    message: "Admin atualizado com sucesso.",
+  });
+};
+
+export const deleteAdmin = async (req, res) => {
+  const { id } = req.params;
+  await instanceAdminService.deleteAdmin(id);
+
+  return res.status(200).json({
+    message: "Admin deletado com sucesso.",
+  });
+};
