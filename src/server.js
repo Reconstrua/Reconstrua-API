d6d4433395998c89ffcd22c0875a2473208285b7
Paulo Ricardo <pr741234@gmail.com>
2024-05-06 08:59:02 -0300

    feat: Configure the express server

--- src/server.js
+++ src/server.js
@@ -1,14 +1,21 @@
+import "express-async-errors";
 import express from "express";
 import { connectToDatabase } from "./database/connection.js";
 import { router } from "./routes/index.routes.js";
+import { errorHandler } from "./middlewares/error/error.middleware.js";
+import { logRoutes } from "./middlewares/utils/log.middleware.js";
+import corsMiddleware from "./middlewares/utils/cors.middleware.js";
 
 export const app = express();
 const port = 4000;
 
 app.use(express.json());
-app.use(router)
+app.use(corsMiddleware);
+app.use(logRoutes);
+app.use(router);
+app.use(errorHandler);
 
 app.listen(port, async () => {
-    connectToDatabase();
-    console.log(`Server run on port: ${port}`)
+  connectToDatabase();
+  console.log(`Server run on port: ${port}`);
 });
