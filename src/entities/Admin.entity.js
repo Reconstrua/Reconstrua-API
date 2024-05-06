e499b812ea52def114899782ed11176b6c5ba711
Paulo Ricardo <pr741234@gmail.com>
2024-05-06 09:07:01 -0300

    feat: Create admin entity

---
+++ src/entities/Admin.entity.js
@@ -0,0 +1,19 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../database/connection.js";
+
+export const Admin = sequelize.define("Admin", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+});
