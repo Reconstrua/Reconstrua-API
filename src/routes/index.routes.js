00614d3b8de682e253f5c156185bbc8f119e365c
Paulo Ricardo <pr741234@gmail.com>
2024-05-06 15:11:03 -0300

    feat: Configure the routers companies  and voluntaries
    Configure the endpoints for companies and voluntaries

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { beneficiaryRouter } from "./beneficiary.routes.js";
 import { adminRouter } from "./admin.routes.js";
+import { companyRouter } from "./company.routes.js";
+import { voluntaryRouter } from "./voluntary.routes.js";
 
 export const router = Router();
 
 router.use("/api/beneficiaries", beneficiaryRouter);
 router.use("/api/admins", adminRouter);
+router.use("/api/companies", companyRouter);
+router.use("/api/voluntaries", voluntaryRouter);
