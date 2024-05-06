fddb16cc325048ce0372409e5215c5ae37e77b05
Paulo Ricardo <pr741234@gmail.com>
2024-05-06 09:08:05 -0300

    feat: Configure sequelize and the database

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,16 +1,14 @@
-import { Sequelize } from 'sequelize';
-import 'dotenv/config';
+import { Sequelize } from "sequelize";
+import "dotenv/config";
 
-const sequelize = new Sequelize(process.env.db_url);
+const sequelize = new Sequelize(process.env.db_url, {
+  logging: false,
+});
 
 const connectToDatabase = async () => {
-  try {
-    await sequelize.authenticate();
-    await sequelize.sync();
-    console.log('Connection has been established successfully');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+  await sequelize.authenticate();
+  await sequelize.sync();
+  console.log("Connection has been established successfully");
 };
 
 export { sequelize, connectToDatabase };
