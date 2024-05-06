0ffe37cfe06b8a80f7d405a084904c5efbce88f0
Paulo Ricardo <pr741234@gmail.com>
2024-05-06 15:12:27 -0300

    feat: Create company entity

--- src/entities/Company.entity.js
+++ src/entities/Company.entity.js
@@ -1,55 +1,57 @@
-import { Sequelize, DataTypes } from "sequelize";
+import { DataTypes } from "sequelize";
 import { sequelize } from "../database/connection.js";
 
-export const Company = sequelize.define('Company', {
-    id: {
-        type: DataTypes.UUID,
-        defaultValue: Sequelize.UUIDV4,
-        primaryKey: true,
-        allowNull: false
-    },
-    company_name: {
-        type: DataTypes.STRING(255),
-        allowNull: false
-    },
-    email: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
-        unique: true
-    },
-    phone: {
-        type: DataTypes.STRING(11),
-        allowNull: false
-    },
-    website: {
-        type: DataTypes.STRING(255)
-    },
-    cep: {
-        type: DataTypes.STRING(8),
-        allowNull: false
-    },
-    street: {
-        type: DataTypes.STRING(255),
-        allowNull: false
-    },
-    neighborhood: {
-        type: DataTypes.STRING(255),
-        allowNull: false
-    },
-    city: {
-        type: DataTypes.STRING(255),
-        allowNull: false
-    },
-    state: {
-        type: DataTypes.STRING(255),
-        allowNull: false
-    },
-    complement: {
-        type: DataTypes.STRING(255),
-        allowNull: false
-    },
-    service: {
-        type: DataTypes.STRING(255),
-        allowNull: false
-    }
+export const Company = sequelize.define("Company", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+    allowNull: false,
+  },
+  company_name: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+    unique: true,
+  },
+  phone: {
+    type: DataTypes.STRING(13),
+    allowNull: false,
+    unique: true,
+  },
+  website: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  cep: {
+    type: DataTypes.STRING(9),
+    allowNull: false,
+  },
+  street: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  neighborhood: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  city: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  state: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  address_number: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
 });
